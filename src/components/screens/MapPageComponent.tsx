d158598855152c5eff43a2d4d20469cb09adbd4b
balendu9 <balendurawat9@gmail.com>
2025-02-26 13:11:54 +0530

    map added

--- src/components/screens/MapPageComponent.tsx
+++ src/components/screens/MapPageComponent.tsx
@@ -1,13 +1,47 @@
+
+
+"use client";// Ensures this runs only on the client side
+
+// 'use client'; // Ensure it runs only on the client side
+
+import { useEffect, useRef, useState} from 'react';
+import { MapContainer } from '../../../node_modules/react-leaflet/lib/MapContainer';
+import { TileLayer } from '../../../node_modules/react-leaflet/lib/TileLayer';
+import { useMap } from '../../../node_modules/react-leaflet/lib/hooks';
+
+
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
 export default function MapPageComponent() {
-    
-    const sample = "This is the screen page to create all the fuzzes"
+  const mapRef = useRef(null);
+  const mapContainerRef = useRef(null);
+  const [mapVisible, setMapVisible] = useState(true);
 
-    return (
-        <div className="w-[800px]  bg-gray-600 text-white font-600 text-[30px] flex justify-center items-center">
-            {sample}
-            <br></br>
-          
-        </div>
-    )
+  useEffect(() => {
+    if (mapVisible && !mapRef.current) {
+      // Initialize map when it becomes visible
+      mapRef.current = L.map(mapContainerRef.current).setView([51.505, -0.09], 13);
+      L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
+        attribution: '&copy; <a href="https://carto.com/">CARTO</a> contributors',
+        subdomains: "abcd",
+        maxZoom: 20,
+      }).addTo(mapRef.current);
+    }
+  }, [mapVisible]); // Runs when mapVisible changes
+
+  return (
+    <div>
+      {/* <button onClick={() => setMapVisible(true)}>Show</button> */}
+      <div
+        ref={mapContainerRef}
+        style={{
+          display: mapVisible ? "block" : "none",
+          width: "100%",
+          height: "90vh",
+          marginTop: "10px",
+        }}
+      />
+    </div>
+  );
 }
-
